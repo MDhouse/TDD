7a61a18608210dcd9edb23b1839568fdf1dbddb7
Damian Junko <damian.junko@hotmail.com>
2017-11-08 17:04:21 +0100

    First Test

--- TddShop/TddShop.Cli.Tests/Account/PasswordValidatorTests.cs
+++ TddShop/TddShop.Cli.Tests/Account/PasswordValidatorTests.cs
@@ -11,6 +11,18 @@ namespace TddShop.Cli.Tests.Account
     [TestFixture]
     public class PasswordValidatorTests
     {
-        
+        [Test]
+        public void IsValid_ContainUpperCase_ShlouldIsTrue()
+        {
+            // Assert 
+            var valid = new PasswordValidator();
+            var password = "AVD";
+
+            // Act
+            var actual = valid.IsValid(password);
+
+            // Assert
+            Assert.That(actual, Is.True);
+        }
     }
 }
